2cadfe861b2fbdb65d6141f780ab0fbd15e80de1
xujingnan <xujingnan308@gmail.com>
2015-05-08 14:22:16 +0800

    add prev button

--- app/src/main/java/com/xujingnan/geoquiz/QuizActivity.java
+++ app/src/main/java/com/xujingnan/geoquiz/QuizActivity.java
@@ -1,7 +1,7 @@
 package com.xujingnan.geoquiz;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,19 +15,22 @@ public class QuizActivity extends ActionBarActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
-    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-        new TrueFalse(R.string.question_oceans, true),
-        new TrueFalse(R.string.question_mideast, false),
-        new TrueFalse(R.string.question_africa, false),
-        new TrueFalse(R.string.question_americas, true),
-        new TrueFalse(R.string.question_asia, true)
+    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+            new TrueFalse(R.string.question_oceans, true),
+            new TrueFalse(R.string.question_mideast, false),
+            new TrueFalse(R.string.question_africa, false),
+            new TrueFalse(R.string.question_americas, true),
+            new TrueFalse(R.string.question_asia, true)
     };
     private int mCurrentIndex = 0;
 
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getQuestion();
-        mQuestionTextView.setText(question);
+        if (mQuestionTextView != null) {
+            mQuestionTextView.setText(question);
+        }
     }
 
     private void checkAnswer(boolean userPressedTrue) {
@@ -79,6 +82,18 @@ public class QuizActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex -= 1;
+                if (mCurrentIndex < 0) {
+                    mCurrentIndex += mQuestionBank.length;
+                }
                 updateQuestion();
             }
         });
